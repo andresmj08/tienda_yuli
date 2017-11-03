802efbbceb6e2e69c0a87b114f68a2625119f04e
andresmj08 <andresmj08@gmail.com>
2017-11-03 00:55:57 -0500

    Primera Carga

---
+++ application/models/producto.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class producto extends CI_Model {
+
+	public function __construct() {
+        parent::__construct();
+	}
+
+
+	public function nuevo($dato){
+		return $this->db->insert('productos',$dato);
+	}
+
+
+public function agregar($nombre, $dato){
+	$this->db->set('cantidad','cantidad'+$dato);
+	$this->db->where('nombre',$nombre);
+		return $this->db->update('productos');
+}
+
+	public function lista() {
+		$this->db->from('productos');
+		$query = $this->db->get();
+		return $query->result_array();
+	}
+
+}
